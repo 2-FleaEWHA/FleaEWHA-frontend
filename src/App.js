77aec78207d8725d295c378d4c913c04196bfa08
nahee-park <jessica396@ewhain.net>
2021-07-25 18:05:05 +0900

    fix: 빌드에러 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Header from "./component/common/Header";
-import Footer from "./component/common/Footer";
-import Main from "./pages/main";
-import Detail from "./pages/detail";
-import ProductInform from "./pages/ProductInform";
-import ProductRegister from "./pages/ProductRegister";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './component/common/Header';
+import Footer from './component/common/Footer';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+import ProductInform from './pages/ProductInform';
+import ProductRegister from './pages/ProductRegister';
 
 function App() {
   return (
