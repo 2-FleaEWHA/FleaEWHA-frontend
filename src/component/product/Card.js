09439c97162df20d0ed24c8397d94d9c6244b353
sua-kim <sua1021606@gmail.com>
2021-08-25 00:04:36 +0900

    feat: 디테일 페이지 퍼블리싱

--- src/component/product/Card.js
+++ src/component/product/Card.js
@@ -1,7 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import BasicImg from '../../asset/basicImg.svg';
 
-function Card() {
-  return <div>카드 하나를 그릴 컴포넌트 입니다.</div>;
+function Card(props) {
+
+  return (
+      <div style={{"display": "inline-flex", "flex-direction": "row", "justify-content": "center"}}>
+          <GoodsList>
+              <Image><img style={{'width':'250px', 'height': '250px'}} src={BasicImg} /></Image>
+              <GoodsDetail>
+                  <Name>{props.title}</Name>
+                  <SalePrice>
+                      <Price>{props.price}원</Price>
+                  </SalePrice>
+              </GoodsDetail>
+          </GoodsList>
+      </div>
+  );
 }
+
+const GoodsList = styled.div`
+width: 100%;
+margin-right: 5%; 
+`
+const Image = styled.div`
+margin-bottom: 1%;
+`
+const GoodsDetail = styled.div`
+text-align: left;
+margin-left: 2%;
+`
+const Name = styled.div`
+font-size: 100%; font-weight: bold;
+margin-bottom: 1%;
+width: 100%;
+`
+const SalePrice = styled.div`
+font-size: 90%;
+margin-bottom: 1%;
+`
+const Price = styled.div`
+display: inline-flex;
+flex-direction: row;
+font-weight: bold;
+color: #5B8767;
+`
 
 export default Card;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,164 @@
 import React from "react";
+import styled from "styled-components";
+import Card from "../component/product/Card";
+
+const product = [
+    {
+        "productID": 1,
+        "title": "testtitle",
+        "categoryName": "의류",
+        "optionName": "판매중",
+        "price": 1000,
+        "files": [
+            {
+                "fileURL": "https://fleaewhabucket.s3.ap-northeast-2.amazonaws.com/2021-08-16T20%3A31%3A55.425560900_ewha.png"
+            }
+        ],
+        "createTime": "2021-08-17 10:27:48"
+    },
+    {
+        "productID": 2,
+        "title": "testtitle",
+        "categoryName": "의류",
+        "optionName": "판매중",
+        "price": 1000,
+        "files": [
+            {
+                "fileURL": "https://fleaewhabucket.s3.ap-northeast-2.amazonaws.com/2021-08-16T20%3A31%3A55.425560900_ewha.png"
+            }
+        ],
+        "createTime": "2021-08-17 10:27:48"
+    },
+    {
+        "productID": 2,
+        "title": "testtitle",
+        "categoryName": "의류",
+        "optionName": "판매중",
+        "price": 1000,
+        "files": [
+            {
+                "fileURL": "https://fleaewhabucket.s3.ap-northeast-2.amazonaws.com/2021-08-16T20%3A31%3A55.425560900_ewha.png"
+            }
+        ],
+        "createTime": "2021-08-17 10:27:48"
+    },
+    {
+        "productID": 2,
+        "title": "testtitle",
+        "categoryName": "의류",
+        "optionName": "판매중",
+        "price": 1000,
+        "files": [
+            {
+                "fileURL": "https://fleaewhabucket.s3.ap-northeast-2.amazonaws.com/2021-08-16T20%3A31%3A55.425560900_ewha.png"
+            }
+        ],
+        "createTime": "2021-08-17 10:27:48"
+    },
+    {
+        "productID": 2,
+        "title": "testtitle",
+        "categoryName": "의류",
+        "optionName": "판매중",
+        "price": 1000,
+        "files": [
+            {
+                "fileURL": "https://fleaewhabucket.s3.ap-northeast-2.amazonaws.com/2021-08-16T20%3A31%3A55.425560900_ewha.png"
+            }
+        ],
+        "createTime": "2021-08-17 10:27:48"
+    }
+]
 
 function Detail() {
-  return <div>더보기 페이지 입니다</div>;
+  /*const [data, product] = useState();
+  useEffect(async () => {
+        try {
+          const response = await axios.get(path);
+          product(response.data);
+        } catch (e) {
+          console.log(e)
+        }
+      }, []
+  )*/
+  return (
+      <div>
+          <SortBar>
+              <DropDown>
+                  <SortOption value={"total"}>전체</SortOption>
+                  <SortOption value={"selling"}>판매중</SortOption>
+                  <SortOption value={"soldOut"}>판매완료</SortOption>
+              </DropDown>
+          </SortBar>
+          <MenuBar>
+              <MenuBtn>전체</MenuBtn><Division>|</Division>
+              <MenuBtn>의류</MenuBtn><Division>|</Division>
+              <MenuBtn>문구</MenuBtn><Division>|</Division>
+              <MenuBtn>가방</MenuBtn><Division>|</Division>
+              <MenuBtn>기념품</MenuBtn><Division>|</Division>
+              <MenuBtn>공동구매</MenuBtn><Division>|</Division>
+              <MenuBtn>나눔</MenuBtn><Division>|</Division>
+              <MenuBtn>생활소품</MenuBtn>
+          </MenuBar>
+          <ProductList>
+              { product ? product.map((goods, i) => {
+                  return (
+                      <CardComponent>
+                          <Card title={goods.title} price={goods.price} />
+                      </CardComponent>
+                  );
+              }):''}
+          </ProductList>
+      </div>
+  );
 }
 
 export default Detail;
+
+const SortBar = styled.div`
+display: inline-flex;
+justify-content: flex-end;
+width: 70%;
+margin-bottom: 1%;
+`
+const DropDown = styled.select.attrs({
+    name: 'sortType'
+})`
+width: 15%;
+padding: 0.5%;
+border: 1px solid #3A3532; border-radius: 5px;
+font-size: 80%;
+cursor:pointer;
+&:focus {outline: none};
+`
+const SortOption = styled.option.attrs({
+    value: (props) => (props.value)
+})`
+  padding: 3px;
+  font-size: 14px;
+`
+const MenuBar = styled.div`
+display: inline-flex;
+justify-content: center;
+width: 70%;
+margin-bottom: 3%;
+`
+const MenuBtn = styled.button`
+border: none; background: none;
+color: #5B8767;
+margin-left: 3%; margin-right: 3%;
+font-size: 100%;
+cursor: pointer;
+`
+const Division = styled.div`
+color: #5B8767;
+`
+const ProductList = styled.div`
+display: inline-flex;
+flex-wrap: wrap;
+width: 70%;
+margin-bottom: 3%;
+`
+const CardComponent = styled.div`
+margin: 1.5%;
+`
