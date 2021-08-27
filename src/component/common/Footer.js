3a6ca2a82a5f73292f5dd23973cb5bf56792bcba
sua-kim <sua1021606@gmail.com>
2021-08-28 01:52:53 +0900

    feat: footer 추가

--- src/component/common/Footer.js
+++ src/component/common/Footer.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from 'styled-components';
 
 function Footer() {
-  return <div style={{'margin-top':'2%'}}>Footer입니다</div>;
+  return (
+      <div>
+        <FooterBar><Hr /></FooterBar>
+        <div style={{'display':'inline-flex', 'width':'80%'}}>
+          <Description>E-프로 2팀 프로젝트</Description>
+        </div>
+        <div style={{'display':'inline-flex', 'width':'80%', 'margin-bottom':'2%'}}>
+          <Description>이화플리: 벗들을 위한 중고거래 서비스</Description>
+        </div>
+      </div>
+  );
 }
 
 export default Footer;
+
+const FooterBar = styled.div`
+display: inline-flex;
+justify-content: center;
+width: 80%; margin-top: 5%; margin-bottom: 1%
+`
+const Hr = styled.div`
+display: inline-flex;
+width: 100%; height: 3px;
+background: #375945;
+`
+const Description = styled.div`
+display: inline-flex;
+width: 100%;
+justify-content: flex-end;
+color: #C7C9CC;
+`
