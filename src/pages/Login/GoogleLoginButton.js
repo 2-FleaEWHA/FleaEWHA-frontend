7fcd7e0bd65fc24d57eaeb82a2e470af223b6bfb
TaeeunKim <xodms0309@naver.com>
2021-08-27 15:06:47 +0900

    feat: 유저 정보 서버와 연결

--- src/pages/Login/GoogleLoginButton.js
+++ src/pages/Login/GoogleLoginButton.js
@@ -1,51 +1,57 @@
 import React,{useEffect, useState} from 'react';
 import {GoogleLogin,GoogleLogout} from 'react-google-login';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
 const clientId='645051058315-he0bjsceiqdi42b4su616b8d7ffjrsqj.apps.googleusercontent.com';
-function GoogleLoginButton(){
+function GoogleLoginButton(props){
     const [isLoggedIn, setLoginState]=useState(false);
-    const [user, setUser]=useState({email:'', name:''});
-    const savedUser=sessionStorage.getItem('key');
-    const savedEmail=sessionStorage.getItem('email');
-    const savedName=sessionStorage.getItem('name');
+    const savedId=sessionStorage.getItem('id');
+
     //로그인 상태 유지
     useEffect(()=>{
-        //console.log(user);
-        if (savedUser) {
+        if (savedId) {
             setLoginState(true);
-            setUser({
-            ...user,
-            email:savedEmail,
-            name:savedName,
-            });
         }
     }, []);
     //로그인 성공시
     const onSuccess = async(response) => {
         setLoginState(true);
-        const { googleId, profileObj : { email, name } } = response;
-        sessionStorage.setItem('key', response.googleId);
+        const { googleId, profileObj : { email, name} } = response;
+        try {
+            const user = await axios.post('/login', null, {params:{
+                'id':googleId,
+                'name':name,            
+                'email':email,
+                }
+            });
+            console.log(user);
+        } catch (e){
+            console.log(e);
+        }
+        sessionStorage.setItem('id', response.googleId);
         sessionStorage.setItem('email', response.profileObj.email);
-        sessionStorage.setItem('name', response.profileObj.name);  
-        setUser({email:response.profileObj.email, name:response.profileObj.name});
+        sessionStorage.setItem('name', response.profileObj.name); 
     }
     const onFailure = (error) => {
         console.log(error);
     }
     const logout=()=>{
         setLoginState(false);
-        setUser({email:'', name:''})
         sessionStorage.clear();
+        window.location.replace("/")
     }
     return(
-        <>
-        <h1>{user.email}</h1>
         <div>
             {isLoggedIn?
+            <>
             <GoogleLogout
                 clientId={clientId}
                 buttonText="Logout"
                 onLogoutSuccess={logout} 
                 />
+            <Link to='/mypage'><MyPageButton>MyPage</MyPageButton></Link>
+            </>
             :
             <GoogleLogin
                 buttonText='Login with Google'
@@ -57,7 +63,19 @@ function GoogleLoginButton(){
             />
             }
         </div>
-        </>
     )
 }
+
+
+const MyPageButton=styled.button`
+cursor: pointer;
+border: none;
+background: #375945;
+font-size: 14px; 
+font-weight: 500;
+height: 43px;
+margin-left:10px;
+border-radius:5px;
+color: #FFFFFF;
+`
 export default GoogleLoginButton;
--- src/pages/Login/MyPage.js
+++ src/pages/Login/MyPage.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
-function MyPage(){
+import React, {useEffect, useState} from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+function MyPage(props){
+    const savedEmail=sessionStorage.getItem('email');
+    const savedName=sessionStorage.getItem('name');
     return(
         <div>
+            <h1>Email: {savedEmail}</h1> 
+            <h1>Name: {savedName}</h1>
         </div>
     );
 }
